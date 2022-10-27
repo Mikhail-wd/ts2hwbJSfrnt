5ea542b0702e78af72b0a1223b6ce2f3f4c32bb6
Mikhail-wd <Supalis913@yahoo.com>
2022-10-27 20:17:08 +0700

    27.10.2022
    task 2.2

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const loadBtn = document.querySelector(".js-load");
 const resultsContainer = document.querySelector(".js-results");
 const searchInput = document.querySelector(".js-input");
 
-document.getElementsByTagName("form")[0].addEventListener("submit", function (event) {
+document.getElementsByTagName("form")[0, 1].addEventListener("submit", function (event) {
   event.preventDefault();
 })
 const startForPlaceholders = document.getElementById("startForUsers");
@@ -23,7 +23,7 @@ loadBtn.addEventListener("click", function (evt) {
 
 
 axios.get("https://jsonplaceholder.typicode.com/users").then(respond =>
-respond.data.forEach(persone => startForPlaceholders.insertAdjacentHTML("afterend", `<div class="response-container">
+  respond.data.forEach(persone => startForPlaceholders.insertAdjacentHTML("afterend", `<div class="response-container">
               <img src="https://source.unsplash.com/random/200x200?sig=${persone.id}">
               <p> Имя: <span>${persone.name}</span></p>
               <p> Город: <span>${persone.address.city}</span></p>
@@ -31,4 +31,47 @@ respond.data.forEach(persone => startForPlaceholders.insertAdjacentHTML("afteren
               <p> Телефон и вебсайт: <span>${persone.phone}</span>, <span>${persone.website}</span></p>
           </div>
           <br>`)
-));
+  ));
+
+// Задание 2.1
+
+const numberInput = document.getElementsByTagName("Input")[1];
+
+numberInput.addEventListener("keyup", function (event) {
+
+  try {
+    if (numberInput.value > 10 || numberInput.value < 5) {
+      document.getElementById("anonce").innerHTML = "Вроде как ошибок нету"
+    } else {
+      throw error
+    }
+  } catch (error) {
+    if (numberInput.value < 10 || numberInput.value1 > 5) {
+      document.getElementById("anonce").innerHTML = "Нет, нет, нет, число должно быть меньше 5 или больше 10 !"
+    } else {
+      document.getElementById("anonce").innerHTML = "Что то не очень похоже на число."
+    }
+  }
+
+
+})
+
+
+//Задание 2.2
+
+async function lotteryTwoZero() {
+
+  console.log("Вы начали игру в версию 2.0")
+
+  const result = await Math.random(0);
+  
+  const promise = await new Promise(function(resolve){
+    if (result>0.5) {
+      resolve (console.log("Вы выйграли."),console.log("Вам заплатили"),console.log("Игра закончена"))
+    } else {
+      resolve (console.log("Вы проиграли"),console.log("Игра закончена"))
+    }    
+  })
+
+}
+lotteryTwoZero()
